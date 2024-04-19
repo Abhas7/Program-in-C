66fa395daced8deffb8f80b1b8fb3a15438a12ea
Abhas7 <167434004+Abhas7@users.noreply.github.com>
2024-04-19 21:55:53 +0530

    Addition of two numbers in C

---
+++ Addition of two numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    // Write a code by Abhas Somkuwar
+    printf("enter a two number a and b: ");
+    scanf("%d %d",&a,&b);
+    printf("Sum= %d",a+b);
+
+    return 0;
+}
